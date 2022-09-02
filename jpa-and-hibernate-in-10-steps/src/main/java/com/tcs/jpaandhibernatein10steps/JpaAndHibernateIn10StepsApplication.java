2c2aa498b0970483c4d25a4357a4b91519bd2bfa
ManideepKumar V V <vv.manideep@gmail.com>
2022-09-02 07:19:54 +0530

    Learn-jpa-hib-basics-projects
    Learn-jpa-hib-basics-projects

---
+++ jpa-and-hibernate-in-10-steps/src/main/java/com/tcs/jpaandhibernatein10steps/JpaAndHibernateIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.jpaandhibernatein10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateIn10StepsApplication.class, args);
+	}
+
+}
---
+++ jpa-and-hibernate-in-10-steps/src/test/java/com/tcs/jpaandhibernatein10steps/JpaAndHibernateIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.jpaandhibernatein10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernateIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
